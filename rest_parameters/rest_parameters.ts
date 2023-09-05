c96a7578b87009dc72d4b93ce747a463eefae0fe
kellynge1ywa <120782457+kellynge1ywa@users.noreply.github.com>
2023-09-05 12:12:38 +0300

    more on functions

---
+++ rest_parameters/rest_parameters.ts
@@ -0,0 +1,14 @@
+/**<------------------------------------> */
+
+/**<------REST PARAMETERS------> */
+
+//We can also define functions that can take unbounded number of arguements using rest parameters.
+//--Rest parameters allows a function to take indefinite number of arguements
+
+//--A rest parameter appears after all other parameters and uses the (...) syntax.
+
+function multiply(x:number,...y:number[]){
+
+}
+
+multiply(2,3,4,5,6,7,)
